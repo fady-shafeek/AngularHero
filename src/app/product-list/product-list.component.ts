deda57700f54197b77a21c8a61814762b57f7041
Fady Shafeek <Fady.Shafeek@hotmail.com>
2022-03-11 20:53:09 +0200

    adding list of products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ICategory } from '../Models/icategory';
+import { IProduct } from '../Models/iproduct';
 
 @Component({
   selector: 'app-product-list',
@@ -7,7 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.prdList=[
+      {id:1, name:'Iphone 6',price:4000, quantity:2, img:'https://picsum.photos/70/70' ,categoryId:1},
+      {id:2, name:'Samsung S21',price:5000, quantity:1, img:'https://picsum.photos/70/70' ,categoryId:2},
+      {id:3, name:'Iphone 11',price:20000, quantity:1, img:'https://picsum.photos/70/70' ,categoryId:1},
+      {id:4, name:'OnePlus 6',price:8000, quantity:2, img:'https://picsum.photos/70/70' ,categoryId:3}
+    ];
+    this.catList=[
+      {id:1,name:'Apple'},
+      {id:2, name:'Samsung'},
+      {id:3, name:'OnePlus'}
+    ]
+  }
+  prdList:IProduct[];
+  catList:ICategory[];
+  selectedCatId:number=0;
+
 
   ngOnInit(): void {
   }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from '../Models/iproduct';
 import { StoredData } from '../ViewModels/stored-data';
 
 @Component({
@@ -11,15 +10,9 @@ export class ProductsComponent implements OnInit {
 
   constructor() { 
     this.storeInfo = new StoredData('Amazon','https://picsum.photos/250/100',['Food','Drink', 'Sport']);
-    this.prdList=[
-      {id:1, name:'Iphone 6',price:4000, quantity:2, img:'https://picsum.photos/70/70' ,categoryId:1},
-      {id:2, name:'Samsung S21',price:5000, quantity:1, img:'https://picsum.photos/70/70' ,categoryId:2},
-      {id:3, name:'Iphone 11',price:20000, quantity:1, img:'https://picsum.photos/70/70' ,categoryId:1},
-      {id:4, name:'OnePlus 6',price:8000, quantity:2, img:'https://picsum.photos/70/70' ,categoryId:3}
-    ];
+
 
   }
-  prdList:IProduct[];
   IsShown: boolean=true;
   storeInfo: StoredData;
   ClientName:string="John Doe"
